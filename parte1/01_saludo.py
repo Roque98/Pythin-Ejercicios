2027994c214f406693a5fcfb8da62a9528039714
Roque Ayala Angel David <ingRoque98@gmail.com>
2020-06-19 23:23:20 -0500

    Ejercio p1 01_saludo Resuelto

---
+++ parte1/01_saludo.py
@@ -0,0 +1,11 @@
+"""
+Saludo:
+    Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+Ejemplo:
+    Ingrese su nombre: Perico
+    Hola, Perico
+"""
+
+if __name__ == "__main__":
+    nombre = input("Ingrese su nombre:")
+    print(f'Hola, {nombre}')
